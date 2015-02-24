a4de3bcbdd044e88638e555b0522347487cf7d20
robinradic <robin@radic.nl>
2015-02-24 20:50:49 +0100

    update @ 24-02-2015 20:50:49

--- update.sh
+++ update.sh
@@ -1,61 +1,76 @@
 #!/bin/bash
 
-# phar-composer
-rm -f phar-composer.phar
-wget http://www.lueck.tv/phar-composer/phar-composer.phar
+function _update {
+    # phar-composer
+    rm -f phar-composer.phar
+    wget http://www.lueck.tv/phar-composer/phar-composer.phar
 
-# pdepend
-rm -f pdepend.phar
-wget http://static.pdepend.org/php/latest/pdepend.phar
+    # pdepend
+    rm -f pdepend.phar
+    wget http://static.pdepend.org/php/latest/pdepend.phar
 
-# phing
-rm -f phing.phar
-wget http://www.phing.info/get/phing-latest.phar
-mv phing-latest.phar phing.phar
+    # phing
+    rm -f phing.phar
+    wget http://www.phing.info/get/phing-latest.phar
+    mv phing-latest.phar phing.phar
 
-# phpab
-rm -f phpab.phar
-wget http://phpab.net/phpab-1.16.1.phar
-mv phpab-1.16.1.phar phpab.phar
+    # phpab
+    rm -f phpab.phar
+    wget http://phpab.net/phpab-1.16.1.phar
+    mv phpab-1.16.1.phar phpab.phar
 
-# phpcpd
-rm -f phpcpd.phar
-wget https://phar.phpunit.de/phpcpd.phar
+    # phpcpd
+    rm -f phpcpd.phar
+    wget https://phar.phpunit.de/phpcpd.phar
 
-# phpcs
-rm -f phpcs.phar
-wget https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar
+    # phpcs
+    rm -f phpcs.phar
+    wget https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar
 
-# phpcbf
-rm -f phpcbf.phar
-wget https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar
-
-
-# phpdox
-rm -f phpdox.phar
-wget http://phpdox.de/releases/phpdox.phar
+    # phpcbf
+    rm -f phpcbf.phar
+    wget https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar
 
 
-# phploc
-rm -f phploc.phar
-wget https://phar.phpunit.de/phploc.phar
+    # phpdox
+    rm -f phpdox.phar
+    wget http://phpdox.de/releases/phpdox.phar
 
 
-# phpmd
-rm -f phpmd.phar
-wget http://static.phpmd.org/php/latest/phpmd.phar
+    # phploc
+    rm -f phploc.phar
+    wget https://phar.phpunit.de/phploc.phar
 
 
-# phpunit
-rm -f phpunit.phar
-wget https://phar.phpunit.de/phpunit.phar
+    # phpmd
+    rm -f phpmd.phar
+    wget http://static.phpmd.org/php/latest/phpmd.phar
 
-# phpdoc
-rm -f phpdoc.phar
-wget http://www.phpdoc.org/phpDocumentor.phar
-mv phpDocumentor.phar phpdoc.phar
 
-rm -f phrocco.phar
-php phar-composer.phar build rossriley/phrocco:dev-master
+    # phpunit
+    rm -f phpunit.phar
+    wget https://phar.phpunit.de/phpunit.phar
 
-chmod +x *.phar
+    # phpdoc
+    rm -f phpdoc.phar
+    wget http://www.phpdoc.org/phpDocumentor.phar
+    mv phpDocumentor.phar phpdoc.phar
+
+    rm -f phrocco.phar
+    php phar-composer.phar build rossriley/phrocco:dev-master
+
+    chmod +x *.phar
+}
+TAG_LAST="$(git describe --abbrev=0 --tags)"
+TAG_PATCH="$(git describe --abbrev=0 --tags | cut -f 3 -d .)"
+TAG_MINOR="$(git describe --abbrev=0 --tags | cut -f 2 -d .)"
+TAG_MAJOR="$(git describe --abbrev=0 --tags | cut -f 1 -d .)"
+
+NEW_TAG="${TAG_MAJOR}.${TAG_MINOR}.$(($TAG_PATCH+1))"
+
+git add -A
+NOW=$(date +"%d-%m-%Y %T")
+git commit -m "update @ ${NOW}"
+git push -u origin master
+
+tag ${NEW_TAG} "${NEW_TAG} @ ${NOW}"
