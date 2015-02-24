be38da3ca660edca335f5e54e31685cc82179622
robinradic <robin@radic.nl>
2015-02-24 21:21:44 +0100

    Tagging v1.2.0

--- tag.sh
+++ tag.sh
@@ -7,7 +7,18 @@ TAG_PATCH="$(git describe --abbrev=0 --tags | cut -f 3 -d .)"
 TAG_MINOR="$(git describe --abbrev=0 --tags | cut -f 2 -d .)"
 TAG_MAJOR="$(git describe --abbrev=0 --tags | cut -f 1 -d .)"
 
-NEW_TAG="${TAG_MAJOR}.$(($TAG_MINOR+1)).0"
+if [ "$1" = "patch" ]; then
+    NEW_TAG="${TAG_MAJOR}.${TAG_MINOR}.$((TAG_PATCH+1))"
+elif [ "$1" = "minor" ]; then
+    NEW_TAG="${TAG_MAJOR}.$(($TAG_MINOR+1)).0"
+elif [ "$1" = "patch" ]; then
+    NEW_TAG="$(($TAG_MAJOR+1)).0.0"
+else
+    echo "Failed. Specify version bump type. Usage: './tag.sh [patch|minor|major]'"
+    exit 1
+fi
+
+
 
 git add -A
 NOW=$(date +"%d-%m-%Y %T")
