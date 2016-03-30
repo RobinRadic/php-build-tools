9b8ab95dce95c59012a9aca0cc258a78b809137f
Robin Radic <robin@radic.nl>
2016-03-31 01:30:24 +0200

    upd

--- install-phar-local.sh
+++ install-phar-local.sh
@@ -1,28 +1,11 @@
 #!/bin/bash
 
+git submodule update --init --remote --recursive --force
+
 MYDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source "${MYDIR}/lib/_lib.sh"
 
 LOCATION="/usr/local/bin"
 
-function _move {
-    local NAME=$1
-    sudo cp -f "${NAME}.phar" "${LOCATION}/${NAME}"
-    sudo chmod +x "${LOCATION}/${NAME}" # just to be sure
-}
-
-_move phar-composer
-_move pdepend
-_move phing
-_move phpab
-_move phpcpd
-_move phpcs
-_move phpcbf
-_move phpdox
-_move phploc
-_move phpmd
-_move phpunit
-_move phpdoc
-_move phrocco
-
-
+sudo chmod +x $MYDIR/bin/*
+sudo cp -f $MYDIR/bin/* $LOCATION/
